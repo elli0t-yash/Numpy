11741aa8fcf2b695022d81cf05d56bd3568ff43a
elli0t-yash <dev.yash.git@gmail.com>
2021-05-05 00:00:28 +0530

    shape and size

--- hello.py
+++ hello.py
@@ -66,3 +66,22 @@ print(np.sort(arr))
 a = np.array([1,3,4,2])
 b = np.array([5,7,8,6])
 print(np.concatenate((a,b)))
+
+# 2d concatenate
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6]])
+print(np.concatenate((x,y), axis = 0))
+
+
+arr_eg = np.array([[[0,1,2,3],
+                    [4,5,6,7]],
+                    
+                    [[0,1,2,3],
+                     [4,5,6,7]],
+                     
+                    [[0,1,2,3],
+                     [4,5,6,7]]])
+# number of dimensions
+print(arr_eg.ndim)
+# size
+print(arr_eg.size)
