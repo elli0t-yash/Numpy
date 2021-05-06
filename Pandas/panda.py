672179cabc6c0f5369263dbc1509e488ca0f3cd7
elli0t-yash <dev.yash.git@gmail.com>
2021-05-06 14:33:30 +0530

    csv/xls

--- Pandas/panda.py
+++ Pandas/panda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openpyxl.workbook import Workbook
 
 df = pd.DataFrame(
     {
@@ -18,3 +19,15 @@ print(df["Age"])
 ages = pd.Series([12, 35, 58], name="Age")
 print(ages)
 
+print(df["Age"].max())
+print(df.describe())
+
+titanic = pd.read_csv("Pandas/titanic.csv")
+print(titanic)
+
+print(titanic.head(8))
+print(titanic.dtypes)
+
+titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
+
+print(titanic.info())
