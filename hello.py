98eadb38b35710ff4d8af9f06231ac81842b198f
elli0t-yash <dev.yash.git@gmail.com>
2021-05-06 12:28:52 +0530

    pandas

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import pandas as pd
 
 # a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
 # print(a[0])
@@ -302,3 +303,17 @@ csv_arr = np.array([1,2,3,4,5,6,7,8])
 np.savetxt('new_file.csv', csv_arr)
 csv = np.loadtxt('new_file.csv')
 print(csv)
+
+a7 = np.array([[-2.58289208,  0.43014843, -1.24082018, 1.59572603],
+               [ 0.99027828, 1.17150989,  0.94125714, -0.14692469],
+               [ 0.76989341,  0.81299683, -0.95068423, 0.11769564],
+               [ 0.20484034,  0.34784527,  1.96979195, 0.51992837]])
+
+df = pd.DataFrame(a7)
+print(df)
+
+df.to_csv('pd.csv')
+data1 = pd.read_csv('pd.csv')
+print(data1)
+
+np.savetxt('np.csv',a7,fmt='%.2f',delimiter=',',header='1, 2, 3, 4')
