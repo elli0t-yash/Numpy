230438fba21b7dd09d3593da2e77ba1763d11af5
elli0t-yash <dev.yash.git@gmail.com>
2021-05-06 16:49:21 +0530

    plot

--- Scikit_Learn/RandomForestClassifier.py
+++ Scikit_Learn/RandomForestClassifier.py
@@ -1,6 +1,26 @@
+import matplotlib.pyplot as plt 
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(random_state=0)
-X = [[1, 2, 3],
-     [11,12,13]]
-Y = [0,1]
-print(clf.fit(X,Y))
+from sklearn.preprocessing import StandardScaler
+
+# clf = RandomForestClassifier(random_state=0)
+# X = [[1, 2, 3],
+#      [11,12,13]]
+# Y = [0,1]
+# print(clf.fit(X,Y))
+# print(clf.predict(X))
+# print(clf.predict([[4, 5, 6],[14, 15, 16]]))
+
+# x = [[0, 15],
+#      [1, -10]]
+# print(StandardScaler().fit(x).transform(x))
+
+
+# plotting graph
+x = [i for i in range(10)]
+y = [2*i for i in range(10)]
+plt.plot(x, y)
+plt.show()
+
+plt.scatter(x,y)
+plt.show()
+
