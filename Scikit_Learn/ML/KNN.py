1588b3b215874076d30f6028304ad8bcfa9b5c5d
elli0t-yash <dev.yash.git@gmail.com>
2021-05-06 20:00:19 +0530

    first ml model

--- Scikit_Learn/ML/KNN.py
+++ Scikit_Learn/ML/KNN.py
@@ -6,3 +6,51 @@ from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('F:\Python\Scikit_Learn\ML\car.data')
 print(data.head())
+
+X = data[[
+    'buying',
+    'maint',
+    'safety'
+]].values
+
+y = data[['class']]
+
+# print(X, y)
+
+# converting the data
+# X
+Le = LabelEncoder()
+for i in range(len(X[0])):
+    X[:,i] = Le.fit_transform(X[:,i])
+
+# print(X)
+
+# y
+label_mapping = {
+    'unacc': 0,
+    'acc': 1,
+    'good': 2,
+    'vgood': 3
+}
+y['class'] = y['class'].map(label_mapping)
+y = np.array(y)
+# print(y)
+
+# create model
+knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+knn.fit(X_train, y_train)
+prediction = knn.predict(X_test)
+accuracy = metrics.accuracy_score(y_test, prediction)
+
+print("Predictions: ", prediction)
+print("Accuracy: ",accuracy)
+
+# print("actual value:", y[20])
+# print("predicted value:", knn.predict(X)[20])
+
+a = 1727
+print("actual value:", y[a])
+print("predicted value:", knn.predict(X)[a])
